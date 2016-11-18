97a42a9c63a2b8c90e86b97dadbc95d58b3cff09
seantking <sean7thomasking@gmail.com>
2016-11-18 18:09:15 +0000

    hidden text field for password/username

--- login/controllers/loginController.js
+++ login/controllers/loginController.js
@@ -1,7 +1,23 @@
 angular.module('basic_framework').controller('loginController', ['LogicLayer' , '$scope', '$location', function(LogicLayer, $scope, $location) {
 
     $scope.login = function(){
-        $location.path('app/form');
+        checkPassword();
+        if(!wrongPassword)
+            $location.path('app/form');
+    };
+
+    $scope.loginDetails = {
+        password: "",
+        username: ""
+    };
+
+    function checkPassword(){
+        if($scope.loginDetails.password === 'password' && $scope.loginDetails.username === 'username')
+            wrongPassword = false;
+        else
+            wrongPassword = true;
     }
+
+   var wrongPassword = false;
 
 }]);
