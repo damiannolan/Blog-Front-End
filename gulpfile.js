c0dff60e08b0e43b19ce51d544cf52f0c6a4d676
seantking <sean7thomasking@gmail.com>
2016-11-27 21:56:44 +0000

    form page style updated

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
 
 var autoprefixerOptions = {
     browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
@@ -15,24 +17,23 @@ var sassOptions = {
 };
 
 gulp.task('sass', function() {
-
-    return gulp.src('stylesheets/stylesheet.scss') //gets all scss files within scss directory or child directories
+    return gulp.src('stylesheets/sass/*.scss') //gets all scss files within scss directory or child directories
         .pipe(sass(sassOptions).on('error', sass.logError)) //using gulp-sass
         .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest('./stylesheets/'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./stylesheets/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
 gulp.task('watch',['browserSync', 'sass'], function(){
-    gulp.watch('stylesheets/*.scss', ['sass', browserSync.reload]);
+    gulp.watch('stylesheets/sass/*.scss', ['sass', browserSync.reload]);
     gulp.watch('form/controllers/*.js', browserSync.reload);
     gulp.watch('form/templates/*.html', browserSync.reload);
     gulp.watch('core/*.js', browserSync.reload);
     gulp.watch('index.html', browserSync.reload);
     gulp.watch('app.js', browserSync.reload);
-    //other watchers
 });
 
 gulp.task('browserSync', function(){
@@ -43,11 +44,4 @@ gulp.task('browserSync', function(){
         port: 8082
     });
 });
-
-
-
-
-
-
-
 
